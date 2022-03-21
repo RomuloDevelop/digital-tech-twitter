e840b2f5f4a1f4a6cad165081d528ae6a0dbcde0
Romulo <romulocg25@gmail.com>
2022-03-21 16:17:08 -0400

    Add visual confirmation of batch's update

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,6 @@ import * as Yup from 'yup';
 import { FormikHelpers, useFormik } from 'formik'; 
 import Header from "../../components/LoginHeader";
 import Card from "../../components/Card";
-import './Login.scss';
 import { RootState, store } from "../../store";
 import { setActualUser, setUsers } from "../../store/user";
 import { useSelector } from "react-redux";
@@ -12,6 +11,9 @@ import { FormEvent, useMemo, useRef, useState } from "react";
 import FormButton from "../../components/Button";
 import Input from "../../components/InputText";
 import { setPosts } from "../../store/post";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Login.scss';
 
 interface LoginFields {username: string}
 
@@ -50,21 +52,46 @@ const Login = () => {
 
   const handleFileChange = (event: FormEvent<HTMLInputElement>) => {
     if (event.currentTarget.files) {
+      const notyConf =  {
+        autoClose: 1500,
+        isLoading: false,
+        hideProgressBar: false,
+        pauseOnHover: true,
+        draggable: true,
+        closeButton: true,
+      }
+      const notifyId = notifyLoad()
       const url = URL.createObjectURL(event.currentTarget.files[0])
       fetch(url)
         .then(response => response.json())
         .then(jsonResponse => {
           const data = jsonResponse as RootState
-          if (data.userReducer) {
-            store.dispatch(setUsers(data.userReducer.users))
-          }
-
-          if (data.postReducer) {
-            store.dispatch(setPosts(data.postReducer))
-          }
-        }) 
+          setTimeout(() => {
+            if (data.postReducer && data.userReducer) {
+              store.dispatch(setUsers(data.userReducer.users))
+              store.dispatch(setPosts(data.postReducer))
+              toast.update(notifyId, { render: 'Data cargada', type: 'success', ...notyConf });
+            } else {
+              toast.update(notifyId, { render: 'Error al cargar la data', type: 'error', ...notyConf });
+            }
+          }, 1500)
+        })
+        .catch((err) => {
+          console.error(err)
+          toast.update(notifyId, { render: 'Error al cargar la data', type: 'error', ...notyConf });
+        })
     }
   }
+
+  const notifyLoad = () => toast('Cargando data', {
+    type: 'info',
+    position: "top-right",
+    pauseOnHover: true,
+    draggable: false,
+    progress: undefined,
+    closeButton: false,
+    isLoading: true
+  });
 
   const addBatch = (event: any) => {
     event.preventDefault()
@@ -72,47 +99,49 @@ const Login = () => {
   }
 
   return (
-    <div className="login-container">
-      <div className="login-wrapper">
-        <div className="header-wrapper">
-          <Header></Header>
-        </div>
-        <Card>
-          <>
-          <h1 className="text-slate-500 login-title">Inicia sesión</h1>
-          {noExist && <span className="login-error text-red-500">El usuario no existe</span>}
-          <form className="flex direction-column" onSubmit={formik.handleSubmit}>
-          <Input
-              placeholder="username"
-              name="username"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.username}
-              error={usernameError}
-              errorMessage={formik.errors.username}
-            ></Input>
+    <>
+      <div className="login-container">
+        <div className="login-wrapper">
+          <div className="header-wrapper">
+            <Header></Header>
+          </div>
+          <Card>
+            <>
+            <h1 className="text-slate-500 login-title">Inicia sesión</h1>
+            {noExist && <span className="login-error text-red-500">El usuario no existe</span>}
+            <form className="flex direction-column" onSubmit={formik.handleSubmit}>
+              <Input
+                placeholder="username"
+                name="username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+                error={usernameError}
+                errorMessage={formik.errors.username}
+              ></Input>
 
-            <div className="login-redirect">
-              <p className="text-center text-slate-500">
-                <span className="mr-2">¿No tienes cuenta?</span>
-                <Link to={'/register'}>Registrate</Link>
-                <span> o </span>
-                <button type="button" onClick={addBatch}
-                  className="no-apperance text-purple-600">
-                    Importa un archivo
-                </button>
-                <input ref={fileInput} value="" className='hidden' onChange={handleFileChange} type="file" accept="application/JSON" />
-              </p>
-              <div className="mt-4 flex justify-end">
-                <FormButton loading={formik.isSubmitting} text="Entrar"/>
+              <div className="login-redirect">
+                <p className="text-center text-slate-500">
+                  <span className="mr-2">¿No tienes cuenta?</span>
+                  <Link to={'/register'}>Registrate</Link>
+                  <span> o </span>
+                  <button type="button" onClick={addBatch}
+                    className="no-apperance text-purple-600">
+                      Importa un archivo
+                  </button>
+                  <input ref={fileInput} value="" className='hidden' onChange={handleFileChange} type="file" accept="application/JSON" />
+                </p>
+                <div className="mt-4 flex justify-end">
+                  <FormButton loading={formik.isSubmitting} text="Entrar"/>
+                </div>
               </div>
-            </div>
-          </form>
-          </>
-        </Card>
+            </form>
+            </>
+          </Card>
+        </div>
       </div>
-    </div>
-    // <Link to="/home">Invoices</Link>
+      <ToastContainer />
+    </>
   )
 }
 
